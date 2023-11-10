9a9875209cbf3093d704af1fe2cafb312cbf21b7
jeyasys <92345767+jeyasys@users.noreply.github.com>
2023-11-10 19:14:31 +0800

    Update maint.sh

--- maint.sh
+++ maint.sh
@@ -4,35 +4,35 @@
 comment="/* That's all, stop editing! Happy publishing. */"
 
 # Check if the line already exists in wp-config.php
-if ! grep -Fxq "$comment" wp-config.php
+if grep -Fxq "$comment" wp-config.php
 then
-    # Use sed to insert the comment before 'require_once ABSPATH . 'wp-settings.php';'
-    # This version accounts for varying amounts of whitespace
+    echo "Happy publishing line is already there."
+else
+    # Insert the comment before 'require_once ABSPATH . 'wp-settings.php';'
     sed -i "/^\s*require_once\s*ABSPATH\s*\.\s*'wp-settings.php';/i $comment" wp-config.php
+    echo "Happy publishing line was not there, but it's added now."
 fi
 
 # Check if the disable auto update block exists
 if grep -q "define( 'AUTOMATIC_UPDATER_DISABLED', true );" wp-config.php; then
     echo "Auto-update is already disabled."
 elif grep -q "define( 'AUTOMATIC_UPDATER_DISABLED', false );" wp-config.php; then
-    # Replace false with true if it's set to false
+    # Replace false with true
     sed -i "s/define( 'AUTOMATIC_UPDATER_DISABLED', false );/define( 'AUTOMATIC_UPDATER_DISABLED', true );/g" wp-config.php
+    echo "Auto-update was enabled, but now it's disabled."
 else
-    # If the code block is missing, add it
+    # Add the code block if missing
     echo "/* Disable all site auto updates  */" >> wp-config.php
     echo "define( 'AUTOMATIC_UPDATER_DISABLED', true );" >> wp-config.php
+    echo "Auto update was not defined, it's disabled now."
 fi
 
-# Install the LiteSpeed Cache plugin
+# Install and activate the LiteSpeed Cache plugin
 wp plugin install litespeed-cache
-
-# Activate the LiteSpeed Cache plugin
 wp plugin activate litespeed-cache
 
-# Install the Flush Opcache plugin
+# Install and activate the Flush Opcache plugin
 wp plugin install flush-opcache
-
-# Activate the Flush Opcache plugin
 wp plugin activate flush-opcache
 
 # Purge all caches in LiteSpeed
@@ -46,6 +46,9 @@ wp redis flush
 
 # Flush rewrite rules (resave permalinks)
 wp rewrite flush --hard
+
+# Notify before deleting the script
+echo "Script will be destroyed now."
 
 # Delete the script itself
 rm -- "$0"
