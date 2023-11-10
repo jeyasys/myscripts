5b730c4c19ad881c4ad185b0cb5981a060db02b8
jeyasys <92345767+jeyasys@users.noreply.github.com>
2023-11-10 13:27:53 +0800

    Update dbsearch.sh

--- dbsearch.sh
+++ dbsearch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Prompt for the old and new domain
-read -p "Enter the old domain without HTTP/HTTPS (e.g., domain.com): " OLD_DOMAIN
-read -p "Enter the new domain without HTTP/HTTPS (e.g., abc.com): " NEW_DOMAIN
+read -p "Enter the old domain without HTTP/HTTPS (e.g., olddomain.com): " OLD_DOMAIN
+read -p "Enter the new domain without HTTP/HTTPS (e.g., newdomain.com): " NEW_DOMAIN
 
 # Check if WP-CLI is available
 if ! command -v wp &> /dev/null
@@ -18,18 +18,26 @@ OLD_URL_WWW_HTTP="http://www.$OLD_DOMAIN"
 OLD_URL_WWW_HTTPS="https://www.$OLD_DOMAIN"
 NEW_URL_HTTPS="https://$NEW_DOMAIN"
 
-# Run WP-CLI search-replace for database entries with HTTP and HTTPS
-RECORDS_HTTPS=$(wp search-replace --all-tables --report-changed-only "$OLD_URL_HTTPS" "$NEW_URL_HTTPS" --precise --quiet --skip-columns=guid)
-RECORDS_HTTP=$(wp search-replace --all-tables --report-changed-only "$OLD_URL_HTTP" "$NEW_URL_HTTPS" --precise --quiet --skip-columns=guid)
-RECORDS_WWW_HTTPS=$(wp search-replace --all-tables --report-changed-only "$OLD_URL_WWW_HTTPS" "$NEW_URL_HTTPS" --precise --quiet --skip-columns=guid)
-RECORDS_WWW_HTTP=$(wp search-replace --all-tables --report-changed-only "$OLD_URL_WWW_HTTP" "$NEW_URL_HTTPS" --precise --quiet --skip-columns=guid)
+# Run WP-CLI search-replace for database entries with HTTP and HTTPS and store results
+RECORDS_HTTPS=$(wp search-replace "$OLD_URL_HTTPS" "$NEW_URL_HTTPS" --all-tables --report-changed-only --skip-columns=guid | grep -o '[0-9]\+' | head -1)
+RECORDS_HTTP=$(wp search-replace "$OLD_URL_HTTP" "$NEW_URL_HTTPS" --all-tables --report-changed-only --skip-columns=guid | grep -o '[0-9]\+' | head -1)
+RECORDS_WWW_HTTPS=$(wp search-replace "$OLD_URL_WWW_HTTPS" "$NEW_URL_HTTPS" --all-tables --report-changed-only --skip-columns=guid | grep -o '[0-9]\+' | head -1)
+RECORDS_WWW_HTTP=$(wp search-replace "$OLD_URL_WWW_HTTP" "$NEW_URL_HTTPS" --all-tables --report-changed-only --skip-columns=guid | grep -o '[0-9]\+' | head -1)
 
-# Output the summary table
-echo "Search and replace summary:"
-printf "%-40s %-40s %-10s\n" "Old URL" "New URL" "Records Updated"
-printf "%-40s %-40s %-10s\n" "$OLD_URL_HTTPS" "$NEW_URL_HTTPS" "$RECORDS_HTTPS"
-printf "%-40s %-40s %-10s\n" "$OLD_URL_WWW_HTTPS" "$NEW_URL_HTTPS" "$RECORDS_WWW_HTTPS"
-printf "%-40s %-40s %-10s\n" "$OLD_URL_HTTP" "$NEW_URL_HTTPS" "$RECORDS_HTTP"
-printf "%-40s %-40s %-10s\n" "$OLD_URL_WWW_HTTP" "$NEW_URL_HTTPS" "$RECORDS_WWW_HTTP"
-
+# Output the summary in the desired format
+echo "To (New URL): $NEW_URL_HTTPS"
+echo "From (Old URLs):"
+echo
+echo "- $OLD_URL_HTTPS"
+echo "$RECORDS_HTTPS"
+echo
+echo "- $OLD_URL_HTTP"
+echo "$RECORDS_HTTP"
+echo
+echo "- $OLD_URL_WWW_HTTPS"
+echo "$RECORDS_WWW_HTTPS"
+echo
+echo "- $OLD_URL_WWW_HTTP"
+echo "$RECORDS_WWW_HTTP"
+echo
 echo "Search and replace completed."
