fee2bbace7298d09c0c1f61d172189f0b0fe1284
jeyasys <92345767+jeyasys@users.noreply.github.com>
2023-11-29 18:19:18 +0800

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -3,15 +3,22 @@
 # Set the log file path with the specified format
 LOG_FILE="rsync_$(date +"%Y%m%d%H%M%S").txt"
 
+# Log all commands and their outputs
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+# Default destination path
+DEFAULT_DEST_PATH="/var/www/webroot/ROOT/"
+
 # Prompt for user input
 read -p "1) Source server IP/hostname: " SOURCE_IP
 read -p "2) Source server SSH username: " SSH_USER
 read -p "3) Source server port (default is 22): " SSH_PORT
 read -p "4) Source server file path: " SOURCE_PATH
-read -p "5) Source server SSH password: " -s SSH_PASSWORD
 echo    # Move to the next line after entering the password
-read -p "6) Destination server path (press Enter for default): /var/www/webroot/ROOT/ " DEST_PATH
-read -p "7) Enter the path to the private key (leave blank if none): " PRIVATE_KEY_PATH
+echo "   Default destination server path: $DEFAULT_DEST_PATH"
+read -p "   Enter custom destination server path (or press Enter to use default): " DEST_PATH_CUSTOM
+DEST_PATH=${DEST_PATH_CUSTOM:-"$DEFAULT_DEST_PATH"}
+read -p "5) Enter the path to the private key (leave blank if none): " PRIVATE_KEY_PATH
 
 # Default to port 22 if no port is specified
 SSH_PORT=${SSH_PORT:-22}
@@ -19,7 +26,7 @@ SSH_PORT=${SSH_PORT:-22}
 # Prompt for excluding files/folders
 EXCLUDE_STRING=""
 while true; do
-    read -p "8) Exclude files/folder (enter 'no' or 'n' to skip): " EXCLUDE_FILE
+    read -p "6) Exclude files/folder (enter 'no' or 'n' to skip): " EXCLUDE_FILE
     if [ "$EXCLUDE_FILE" == "no" ] || [ "$EXCLUDE_FILE" == "n" ]; then
         break
     fi
@@ -33,20 +40,7 @@ else
     SSH_COMMAND="ssh -p $SSH_PORT"
 fi
 
-# Default destination path if not provided
-DEST_PATH=${DEST_PATH:-"/var/www/webroot/ROOT/"}
-
 # Display the rsync command to be executed
-RSYNC_COMMAND="rsync -avzhe --progress --stats -e '$SSH_COMMAND' $EXCLUDE_STRING $SSH_USER@$SOURCE_IP:$SOURCE_PATH $DEST_PATH"
+RSYNC_COMMAND="rsync -avzhe --progress --stats --log-file='$LOG_FILE' -e '$SSH_COMMAND' $EXCLUDE_STRING $SSH_USER@$SOURCE_IP:$SOURCE_PATH $DEST_PATH"
 
 echo "Rsync Command: $RSYNC_COMMAND"
-
-# Ask for confirmation
-read -p "Do you want to proceed with this rsync command? (Y/N): " CONFIRMATION
-if [ "$CONFIRMATION" == "Y" ] || [ "$CONFIRMATION" == "y" ]; then
-    # Run the rsync command and log output
-    echo "$SSH_PASSWORD" | $RSYNC_COMMAND > "$LOG_FILE" 2>&1
-    echo "Rsync completed. Log file: $LOG_FILE"
-else
-    echo "Rsync operation aborted."
-fi
