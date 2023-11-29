26200fab6129bdb6410e6742910fbea357d46700
jeyasys <92345767+jeyasys@users.noreply.github.com>
2023-11-29 18:40:38 +0800

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Set the log file path with the specified format
 LOG_FILE="rsync_$(date +"%Y%m%d%H%M%S").txt"
 
-# Log all commands and their outputs
 exec > >(tee -a "$LOG_FILE") 2>&1
 
-# Default destination path
 DEFAULT_DEST_PATH="/var/www/webroot/ROOT/"
 
-# Prompt for user input
 read -p "1) Source server IP/hostname: " SOURCE_IP
 read -p "2) Source server SSH username: " SSH_USER
 read -p "3) Source server port (default is 22): " SSH_PORT
@@ -18,10 +14,8 @@ read -p "5) Enter custom destination server path (or press Enter to use default)
 DEST_PATH=${DEST_PATH_CUSTOM:-"$DEFAULT_DEST_PATH"}
 read -p "6) Enter the path to the private key (leave blank if none): " PRIVATE_KEY_PATH
 
-# Default to port 22 if no port is specified
 SSH_PORT=${SSH_PORT:-22}
 
-# Prompt for excluding files/folders
 EXCLUDE_STRING=""
 while true; do
     read -p "7) Exclude files/folder (enter 'no' or 'n' to skip): " EXCLUDE_FILE
@@ -31,7 +25,6 @@ while true; do
     EXCLUDE_STRING="$EXCLUDE_STRING --exclude '$EXCLUDE_FILE'"
 done
 
-# Determine SSH command based on private key presence
 if [ -n "$PRIVATE_KEY_PATH" ]; then
     SSH_COMMAND="ssh -i $PRIVATE_KEY_PATH -p $SSH_PORT"
 else
