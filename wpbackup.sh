4b2e9982cc71e7b44c4b867db85d6f3f9684b1df
jeyasys <92345767+jeyasys@users.noreply.github.com>
2024-02-09 10:29:21 +0800

    Update wpbackup.sh

--- wpbackup.sh
+++ wpbackup.sh
@@ -10,14 +10,12 @@ DB_USER=$(grep DB_USER wp-config.php | cut -d "'" -f 4)
 DB_PASSWORD=$(grep DB_PASSWORD wp-config.php | cut -d "'" -f 4)
 DB_NAME=$(grep DB_NAME wp-config.php | cut -d "'" -f 4)
 
-
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 
 # Backup the database
 mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > db_$TIMESTAMP.sql
 
 echo "Backing up database now"
-
 
 if [ $? -eq 0 ]; then
     echo "Database backup complete"
@@ -28,11 +26,14 @@ fi
 
 echo "Compressing files"
 
-# Compress WordPress files and database dump, excluding the backup script
 # The archive will be saved in the directory one level above
 tar -czf ../backup_$TIMESTAMP.tar.gz --exclude=$SCRIPTNAME * db_$TIMESTAMP.sql
 
 echo "Archive saved as backup_$TIMESTAMP.tar.gz in /var/www/webroot/"
+
+# After successfully creating the archive, delete the SQL file
+rm db_$TIMESTAMP.sql
+echo "Temporary database backup file db_$TIMESTAMP.sql deleted."
 
 echo "Script will be destroyed now."
 
