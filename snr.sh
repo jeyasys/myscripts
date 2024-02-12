7a1cc764c5171d58b178085fd35d242ade110177
jeyasys <92345767+jeyasys@users.noreply.github.com>
2024-02-12 13:08:58 +0800

    Create snr.sh

---
+++ snr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+read -p "Enter the old directory path: " old_path
+
+new_path="/var/www/webroot/ROOT"
+
+if [[ -z "$old_path" ]]; then
+    echo "You must provide an old directory path."
+    exit 1
+fi
+
+echo "The script will run the following command:"
+echo "wp search-replace \"$old_path\" \"$new_path\" --all-tables"
+
+read -p "Are you sure you want to proceed? (Y/N): " confirmation
+
+confirmation=${confirmation^^}
+
+if [[ "$confirmation" == "Y" ]]; then
+
+    wp search-replace "$old_path" "$new_path" --all-tables
+
+    if [ $? -eq 0 ]; then
+        echo "Search and replace completed successfully."
+    else
+        echo "There was an error performing the search and replace."
+    fi
+else
+    echo "Operation cancelled."
+fi
+
+echo "Script will be destroyed now."
+
+rm -- "$0"
