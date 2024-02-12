62548631900f9fd82736d3b6aeb2ffe9ebdd680a
jeyasys <92345767+jeyasys@users.noreply.github.com>
2024-02-12 13:52:47 +0800

    Update dbsearch.sh

--- dbsearch.sh
+++ dbsearch.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-
 read -p "Enter the old domain without HTTP/HTTPS (e.g., domain.com): " OLD_DOMAIN
 read -p "Enter the new domain without HTTP/HTTPS (e.g., abc.com): " NEW_DOMAIN
 
-# Check if WP-CLI is available
 if ! command -v wp &> /dev/null
 then
     echo "WP-CLI could not be found. Please install WP-CLI and run this script again."
     exit 1
 fi
-
 
 OLD_URL_HTTP="http://$OLD_DOMAIN"
 OLD_URL_HTTPS="https://$OLD_DOMAIN"
@@ -18,29 +15,24 @@ OLD_URL_WWW_HTTP="http://www.$OLD_DOMAIN"
 OLD_URL_WWW_HTTPS="https://www.$OLD_DOMAIN"
 NEW_URL_HTTPS="https://$NEW_DOMAIN"
 
-# Run WP-CLI search-replace for database entries with HTTP and HTTPS
-RECORDS_HTTPS=$(wp search-replace "$OLD_URL_HTTPS" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --quiet | grep -o '[0-9]\+' | head -1)
-RECORDS_HTTP=$(wp search-replace "$OLD_URL_HTTP" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --quiet | grep -o '[0-9]\+' | head -1)
-RECORDS_WWW_HTTPS=$(wp search-replace "$OLD_URL_WWW_HTTPS" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --quiet | grep -o '[0-9]\+' | head -1)
-RECORDS_WWW_HTTP=$(wp search-replace "$OLD_URL_WWW_HTTP" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --quiet | grep -o '[0-9]\+' | head -1)
-
-wp cache flush
-
 echo "To (New URL): $NEW_URL_HTTPS"
 echo "From (Old URLs):"
 echo
+
 echo "- $OLD_URL_HTTPS"
-echo "$RECORDS_HTTPS"
-echo
+wp search-replace "$OLD_URL_HTTPS" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --format=count
+
 echo "- $OLD_URL_HTTP"
-echo "$RECORDS_HTTP"
-echo
+wp search-replace "$OLD_URL_HTTP" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --format=count
+
 echo "- $OLD_URL_WWW_HTTPS"
-echo "$RECORDS_WWW_HTTPS"
-echo
+wp search-replace "$OLD_URL_WWW_HTTPS" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --format=count
+
 echo "- $OLD_URL_WWW_HTTP"
-echo "$RECORDS_WWW_HTTP"
-echo
+wp search-replace "$OLD_URL_WWW_HTTP" "$NEW_URL_HTTPS" --all-tables --skip-columns=guid --format=count
+
+wp cache flush
+
 echo "Search and replace completed."
-# Delete the script file
+
 rm -- "$0"
