ebcf7ba3d893c14ad833758888935a1e1f12cf80
jeyasys <92345767+jeyasys@users.noreply.github.com>
2025-05-12 11:16:27 +0800

    Create access.sh

---
+++ access.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+users=$(rapyd site list | awk '/^\|/{if ($5 ~ /^web_/) print $5}' | sort -u)
+IFS=$'\n' read -rd '' -a user_array <<<"$users"
+if [ ${#user_array[@]} -eq 0 ]; then
+  echo "No users found."
+  rm -- "$0"
+  exit 1
+fi
+echo "Choose the user you'd like to login as:"
+select selected_user in "${user_array[@]}"; do
+  if [[ -n "$selected_user" ]]; then
+    echo "Switching to user: $selected_user"
+    sudo -u "$selected_user" -i
+    break
+  else
+    echo "Invalid selection. Try again."
+  fi
+done
+rm -- "$0"
