3f51d4eee66f3743bbca061b87d4ec84fb5fb333
jeyasys <92345767+jeyasys@users.noreply.github.com>
2025-05-12 11:18:23 +0800

    Update access.sh

--- access.sh
+++ access.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-users=$(rapyd site list | awk '/^\|/{if ($5 ~ /^web_/) print $5}' | sort -u)
+users=$(rapyd site list | awk -F'|' 'NF > 4 && $4 ~ /web_/ { gsub(/ /, "", $4); print $4 }' | sort -u)
 IFS=$'\n' read -rd '' -a user_array <<<"$users"
+
 if [ ${#user_array[@]} -eq 0 ]; then
   echo "No users found."
   rm -- "$0"
   exit 1
 fi
+
 echo "Choose the user you'd like to login as:"
 select selected_user in "${user_array[@]}"; do
   if [[ -n "$selected_user" ]]; then
@@ -17,4 +19,5 @@ select selected_user in "${user_array[@]}"; do
     echo "Invalid selection. Try again."
   fi
 done
+
 rm -- "$0"
